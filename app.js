cbd76cc4b74b8576cc728844e37759edd1408157
Alex Fallenstedt <a.fallenstedt@gmail.com>
2017-06-22 14:49:25 -0700

    create a tweet message after fetching data

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 require('dotenv').config({path: './config/.env'})
 const currencyInfo = require('./js/currencyInfo');
 const poster = require('./js/twitterPoster')
-// Do a thing with currency info.  WOOOOO YEAH!
 
-//fetch info
-//format info
-//
 currencyInfo.fetchCoinInfo(logStuff);
 
 function logStuff(err, formattedData) {
-  console.log('It works!', formattedData);
+  console.log(formattedData);
 }
--- js/currencyInfo.js
+++ js/currencyInfo.js
@@ -5,7 +5,8 @@ const currencyInfo = {
   fetchCoinInfo: (callback) => {
     async.waterfall([
       currencyApi.makeRequest,
-      currencyInfo.formatResponse
+      currencyInfo.formatResponse,
+      currencyInfo.createTweet
     ], callback);
   },
   formatResponse: (data, callback) => {
@@ -31,7 +32,48 @@ const currencyInfo = {
     };
 
     callback(null, data);
+  },
+  createTweet: (data, callback) => {
+    if (!data) {
+      throw new Error("Data is undefined!");
+    }
+
+    const {id, symbol, price_usd, percent_change_7d, percent_change_24h} = data;
+
+    let usd = round(+price_usd, 2);
+    let arrow_24h = currencyInfo.upOrDownEmoji(percent_change_24h);
+    let arrow_7d = currencyInfo.upOrDownEmoji(percent_change_7d);
+
+    const message = `#${id} is valued at $${usd}. Last 24h: ${percent_change_24h}%${arrow_24h} Last 7d: ${percent_change_7d}%${arrow_7d} #${symbol}`;
+
+    callback(null, message);
+  },
+  upOrDownEmoji: (stringNum) => {
+    let num = parseInt(stringNum);
+
+    //return an up arrow or down arrow
+    var emoji = (num >= 0) ? '⬆︎' : '⬇︎'
+    return emoji;
   }
+}
+
+function round(value, exp) {
+  if (typeof exp === 'undefined' || +exp === 0)
+    return Math.round(value);
+
+  value = +value;
+  exp = +exp;
+
+  if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0))
+    return NaN;
+
+  // Shift
+  value = value.toString().split('e');
+  value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp)));
+
+  // Shift back
+  value = value.toString().split('e');
+  return +(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp));
 }
 
 
