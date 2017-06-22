4b2454ee29f7065ad1c1bd06cc172866863da572
Weston Wedding <weston@sticky.tv>
2017-06-22 10:55:26 -0700

    Specify where the .env lives.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config({path: './config/.env'})
 const currencyInfo = require('./js/currencyInfo');
-
+const poster = require('./js/twitterPoster')
 // Do a thing with currency info.  WOOOOO YEAH!
