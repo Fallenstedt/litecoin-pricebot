a979753b7dc1d4fd9a5dd29bd9300bdb37b47152
Alex Fallenstedt <a.fallenstedt@gmail.com>
2017-06-22 09:40:29 -0700

    write https request to obtain LTC data from coinmarketcap

--- js/currencyApi.js
+++ js/currencyApi.js
@@ -1,1 +1,23 @@
-// Do currency API magic heres!
+var https = require('https');
+
+const currencyApi = {
+  makeRequest: () => {
+    https.get("https://api.coinmarketcap.com/v1/ticker/litecoin/", (response) => {
+      var body = "";
+      response.on("data", (data) => {
+        console.log("Request was made...");
+        body += data;
+      });
+      response.on("end", () => {
+        var parsed = JSON.parse(body);
+        console.log(parsed);
+      });
+      response.on("error", (error) => {
+        console.error(error)
+      });
+    });
+  },
+
+}
+
+module.exports = currencyApi
