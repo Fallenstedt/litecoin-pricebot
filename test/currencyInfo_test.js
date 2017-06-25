b8ed2888705d184a324bfa1fecfe62664dc57fc7
Fallenstedt <a.fallenstedt@gmail.com>
2017-06-24 21:46:28 -0700

    unit test the formation of arrow because I was a dum dum

--- test/currencyInfo_test.js
+++ test/currencyInfo_test.js
@@ -1,35 +1,25 @@
+// jshint esversion: 6
 const currencyInfo = require('../js/currencyInfo');
 const dummyData = require('../js/dummyData');
 
 const expect = require('chai').expect;
 
 describe("currencyInfo ", ()=>{
-  describe("it should parse an array for desired data", (done) => {
-    var result;
-
-    beforeEach((done) => {
-      var callback = function(err, result) {
-        if (err) { console.error(err);}
-        return result;
-      }
-      result = currencyInfo.formatResponse(dummyData, callback);
-      done();
-    })
-    
-    xit("returns an function", (done)=>{
-      console.log(result);
-      expect(result).to.be.a('function');
+  describe("forming the tweet", (done) => {
+    it("should use an up arrow if a value is greater than zero", (done) => {
+      var arrow = currencyInfo.upOrDownEmoji("0.5");
+      expect(arrow).to.equal("⬆︎");
       done();
     });
-
-    xit("returns an function with values in each key", (done) => {
-      var values = Object.keys(result).map((key) => {
-        return result[key];
-      });
-      expect(values).to.not.include.undefined;
+    it("should use a down arrow if a value is less than zero", (done) => {
+      var arrow = currencyInfo.upOrDownEmoji("-0.5");
+      expect(arrow).to.equal("⬇︎");
       done();
     });
-
-
+    it("should return an empty string if the value is zero", (done) => {
+      var arrow = currencyInfo.upOrDownEmoji("0");
+      expect(arrow).to.equal("");
+      done();
+    });
   });
 });
